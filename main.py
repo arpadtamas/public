ef7c3da2ce544b4a932b60fd042535d385d88f20
arpadtamas <91497369+arpadtamas@users.noreply.github.com>
2022-09-15 11:34:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-import pefile, ssdeep, sys, re, os
+import pefile, ssdeep, sys, re, os, subprocess, json
 from datetime import datetime
 from filehash import FileHash
 from pathlib import Path
+
+if len(sys.argv) > 1:
+	file = str(sys.argv[1])
+else:
+	print("\tUsage: " + sys.argv[0] + " sample.exe\n")
+	exit()
+	
 
 class selfhash:
 	def __init__(self, file):
@@ -49,6 +56,7 @@ class showapi:
 				if imp.name is not None:
 					imps = re.findall(r"'([^']*)'", str(imp.name))
 					apis.append(imps[0])
+					
 		crypt = ["CryptAcquireContextA", "EncryptFileA", "CryptEncrypt", "CryptDecrypt", "CryptCreateHash", "CryptHashData", "CryptDeriveKey", "CryptSetKeyParam", "CryptGetHashParam", "CryptSetKeyParam", "CryptDestroyKey", "CryptGenRandom", "DecryptFileA", "FlushEfsCache", "GetLogicalDrives", "GetDriveTypeA", "CryptStringToBinary", "CryptBinaryToString", "CryptReleaseContext", "CryptDestroyHash", "EnumSystemLocalesA"]
 		enum = ["CreateToolhelp32Snapshot", "EnumDeviceDrivers", "EnumProcesses", "EnumProcessModules", "EnumProcessModulesEx", "FindFirstFileA", "FindNextFileA", "GetLogicalProcessorInformation", "GetLogicalProcessorInformationEx", "GetModuleBaseNameA", "GetSystemDefaultLangId", "GetVersionExA", "GetWindowsDirectoryA", "IsWoW64Process", "Module32First", "Module32Next", "Process32First", "Process32Next", "ReadProcessMemory", "Thread32First", "Thread32Next", "GetSystemDirectoryA", "GetSystemTime", "ReadFile", "GetComputerNameA", "VirtualQueryEx", "GetProcessIdOfThread", "GetProcessId", "GetCurrentThread", "GetCurrentThreadId", "GetThreadId", "GetThreadInformation", "GetCurrentProcess", "GetCurrentProcessId", "SearchPathA", "GetFileTime", "GetFileAttributesA", "LookupPrivilegeValueA", "LookupAccountNameA", "GetCurrentHwProfileA", "GetUserNameA", "RegEnumKeyExA", "RegEnumValueA", "RegQueryInfoKeyA", "RegQueryMultipleValuesA", "RegQueryValueExA", "NtQueryDirectoryFile", "NtQueryInformationProcess", "NtQuerySystemEnvironmentValueEx", "EnumDesktopWindows", "EnumWindows", "NetShareEnum", "NetShareGetInfo", "NetShareCheck", "GetAdaptersInfo", "PathFileExistsA", "GetNativeSystemInfo", "RtlGetVersion", "GetIpNetTable", "GetLogicalDrives", "GetDriveTypeA", "RegEnumKeyA", "WNetEnumResourceA", "WNetCloseEnum", "FindFirstUrlCacheEntryA", "FindNextUrlCacheEntryA", "WNetAddConnection2A", "WNetAddConnectionA", "EnumResourceTypesA", "EnumResourceTypesExA", "GetSystemTimeAsFileTime", "GetThreadLocale", "EnumSystemLocalesA2"]
 		inj = ["CreateFileMappingA" "CreateProcessA", "CreateRemoteThread", "CreateRemoteThreadEx", "GetModuleHandleA", "GetProcAddress", "GetThreadContext", "HeapCreate", "LoadLibraryA", "LoadLibraryExA", "LocalAlloc", "MapViewOfFile", "MapViewOfFile2", "MapViewOfFile3", "MapViewOfFileEx", "OpenThread", "Process32First", "Process32Next", "QueueUserAPC", "ReadProcessMemory", "ResumeThread", "SetProcessDEPPolicy", "SetThreadContext", "SuspendThread", "Thread32First", "Thread32Next", "Toolhelp32ReadProcessMemory", "VirtualAlloc", "VirtualAllocEx", "VirtualProtect", "VirtualProtectEx", "WriteProcessMemory", "VirtualAllocExNuma", "VirtualAlloc2", "VirtualAlloc2FromApp", "VirtualAllocFromApp", "VirtualProtectFromApp", "CreateThread", "WaitForSingleObject", "OpenProcess", "OpenFileMappingA", "GetProcessHeap", "GetProcessHeaps", "HeapAlloc", "HeapReAlloc", "GlobalAlloc", "AdjustTokenPrivileges", "CreateProcessAsUserA", "OpenProcessToken", "CreateProcessWithTokenW", "NtAdjustPrivilegesToken", "NtAllocateVirtualMemory", "NtContinue", "NtCreateProcess", "NtCreateProcessEx", "NtCreateSection", "NtCreateThread", "NtCreateThreadEx", "NtCreateUserProcess", "NtDuplicateObject", "NtMapViewOfSection", "NtOpenProcess", "NtOpenThread", "NtProtectVirtualMemory", "NtQueueApcThread", "NtQueueApcThreadEx", "NtQueueApcThreadEx2", "NtReadVirtualMemory", "NtResumeThread", "NtUnmapViewOfSection", "NtWaitForMultipleObjects", "NtWaitForSingleObject", "NtWriteVirtualMemory", "RtlCreateHeap", "LdrLoadDll", "RtlMoveMemory", "RtlCopyMemory", "SetPropA", "WaitForSingleObjectEx", "WaitForMultipleObjects", "WaitForMultipleObjectsEx", "KeInsertQueueApc", "Wow64SetThreadContext", "NtSuspendProcess", "NtResumeProcess", "DuplicateToken", "NtReadVirtualMemoryEx", "CreateProcessInternal", "EnumSystemLocalesA", "UuidFromStringA"]
@@ -101,3 +109,92 @@ class showapi:
 				print("\t", i)
 				
 
+class capait:
+	def __init__(self, file):
+
+		output = subprocess.check_output("capa -q -j " + file, shell=True).decode('ascii')
+		data = json.loads(output)
+
+		mbc = []
+		
+		for rulz in data['rules']:
+			print("\nRule match:", rulz)
+			try:
+				if data['rules'][rulz]['meta']['mbc'][0]['behavior'] not in mbc:
+					mbc.append(data['rules'][rulz]['meta']['mbc'][0]['behavior'])
+			except Exception:
+				pass
+			try:
+				scope = data['rules'][rulz]['meta']['scope']
+			except:
+				scope  = None
+			try:
+				offset = hex(data['rules'][rulz]['matches'][0][0]['value'])
+			except:
+				offset = None
+			try:
+				tactic = data['rules'][rulz]['meta']['attack'][0]['tactic']
+			except:
+				tactic = None
+			try:
+				tech = data['rules'][rulz]['meta']['attack'][0]['technique']
+			except:
+				tech = None
+			try:
+				subtech = data['rules'][rulz]['meta']['attack'][0]['subtechnique']
+			except:
+				subtech = None
+			try:
+				mitre = data['rules'][rulz]['meta']['attack'][0]['id']
+			except:
+				mitre = None
+			
+			if scope is not None:
+				print("\tScope:", scope)
+			if offset is not None:
+				print("\tMatches:", offset)
+			if tactic is not None:
+				print("\tTactic:", tactic)
+			if tech is not None:
+				print("\tTechnique:", tech)
+			if subtech and subtech is not None:
+				print("\tSubtechnique:", subtech)
+			if mitre is not None:
+				print("\tMITRE:", mitre)
+			
+		print("-----------\ncapa Malware Behavior Catalog:")
+
+		for behave in mbc:
+			print("\t", behave)
+			
+			
+
+print("""
+    ____            __        ___                
+   / __ \___  _____/ /_____ _/ (_)___  ___  _____
+  / /_/ / _ \/ ___/ __/ __ `/ / /_  / / _ \/ ___/
+ / _, _/  __/ /__/ /_/ /_/ / / / / /_/  __/ /    
+/_/ |_|\___/\___/\__/\__,_/_/_/ /___/\___/_/     
+""")
+                                                
+
+
+
+selfhash(file)
+showapi(file)
+print("""
+
+
+                (`.
+                 \ `.
+                  )  `._..---._
+\`.       __...---`         o  )
+ \ `._,--'           ,    ___,'
+  ) ,-._          \  )   _,-'
+ /,'    ``--.._____\/--''
+
+
+
+Capa rulz:""")
+capait(file)
+#movefile(file)			
